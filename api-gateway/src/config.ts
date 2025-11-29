49ae7991d0b94e7e96fc880c5d1d1777512926ee
JERTOLE <info@cryogas.kz>
2025-11-30 02:26:41 +0300

    Update config.ts

--- api-gateway/src/config.ts
+++ api-gateway/src/config.ts
@@ -1,1 +1,9 @@
+import dotenv from "dotenv";
 
+dotenv.config();
+
+export const config = {
+  port: Number(process.env.API_PORT || 4000),
+  rpcUrl: process.env.JERT_RPC_URL || "http://127.0.0.1:8545",
+  networkChainId: Number(process.env.JERT_CHAIN_ID || 7777)
+};
