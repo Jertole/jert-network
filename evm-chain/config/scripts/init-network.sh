0db35a974d7e0c674265c787c1d013ba171116b3
JERTOLE <info@cryogas.kz>
2025-11-30 00:28:04 +0300

    Update init-network.sh

--- evm-chain/config/scripts/init-network.sh
+++ evm-chain/config/scripts/init-network.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+set -e
 
+echo ">>> Initialising JERT Permissioned EVM network..."
+
+# create data dirs
+mkdir -p ./data/validator1 ./data/validator2 ./data/validator3 ./data/validator4 ./data/validator5 ./data/observer
+
+echo "Data directories created."
+
+echo "Starting Docker containers..."
+docker-compose up -d
+
+echo "JERT network bootstrap request sent. Check container logs:"
+echo "  docker logs -f jert-validator1"
