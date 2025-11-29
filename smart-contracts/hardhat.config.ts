4a1e32af24c62ccb6a55d2d649cd5969f3abf47e
JERTOLE <info@cryogas.kz>
2025-11-30 01:39:14 +0300

    Update hardhat.config.ts

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,1 +1,24 @@
 
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    // локальная/дев сеть JERT (private EVM)
+    jert: {
+      url: "http://127.0.0.1:8545", // поменяете под свой RPC gateway
+      chainId: 7777
+      // accounts: [ "0x..." ]  // добавят девы через .env
+    }
+  }
+};
+
