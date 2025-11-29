9541a974f24b46cf8e4005d9c08714b538ad522b
JERTOLE <info@cryogas.kz>
2025-11-30 02:34:43 +0300

    Update aml-check.ts

--- api-gateway/compliance-middleware/aml-check.ts
+++ api-gateway/compliance-middleware/aml-check.ts
@@ -1,1 +1,10 @@
 
+import { Request, Response, NextFunction } from "express";
+
+/**
+ * Middleware-заглушка для AML (санкции, источники средств и т.д.).
+ */
+export function amlCheck(req: Request, res: Response, next: NextFunction) {
+  // TODO: реальная AML логика
+  return next();
+}
