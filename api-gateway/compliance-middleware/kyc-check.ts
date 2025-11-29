57a539af93f85d1d2341d0f73f3e7538bdfaea75
JERTOLE <info@cryogas.kz>
2025-11-30 02:33:39 +0300

    Update kyc-check.ts

--- api-gateway/compliance-middleware/kyc-check.ts
+++ api-gateway/compliance-middleware/kyc-check.ts
@@ -1,1 +1,10 @@
+import { Request, Response, NextFunction } from "express";
 
+/**
+ * Middleware-заглушка. Далее здесь будет проверка адреса/пользователя по внешнему KYC-сервису.
+ */
+export function kycCheck(req: Request, res: Response, next: NextFunction) {
+  // TODO: реальная проверка
+  // пока просто пропускаем:
+  return next();
+}
