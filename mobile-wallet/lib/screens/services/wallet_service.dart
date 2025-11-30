7894ab91d1844ee6e2328c339868e85f19a2dcad
JERTOLE <info@cryogas.kz>
2025-11-30 15:41:09 +0300

    Update wallet_service.dart

--- mobile-wallet/lib/screens/services/wallet_service.dart
+++ mobile-wallet/lib/screens/services/wallet_service.dart
@@ -1,1 +1,18 @@
+import 'local_storage.dart';
 
+class WalletService {
+  WalletService._();
+  static final WalletService instance = WalletService._();
+
+  static const _addressKey = 'wallet_address';
+
+  Future<String?> getCurrentAddress() async {
+    return LocalStorage.instance.getString(_addressKey);
+  }
+
+  Future<void> setCurrentAddress(String address) async {
+    await LocalStorage.instance.setString(_addressKey, address);
+  }
+
+  // TODO: сюда можно добавить генерацию ключей и импорт seed-фразы
+}
