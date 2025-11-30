a2b4394aa6287d2043fbe863985c7de2fd6c2606
JERTOLE <info@cryogas.kz>
2025-11-30 03:07:06 +0300

    Update App.tsx

--- corporate-wallet/src/App.tsx
+++ corporate-wallet/src/App.tsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { Layout } from "./components/Layout";
+import { MultisigDashboard } from "./components/MultisigDashboard";
+import { TransactionsTable } from "./components/TransactionsTable";
 
+export const App: React.FC = () => {
+  return (
+    <Layout>
+      <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+        <MultisigDashboard />
+        <TransactionsTable />
+      </div>
+    </Layout>
+  );
+};
