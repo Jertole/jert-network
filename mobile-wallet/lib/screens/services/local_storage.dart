47824e1260c6c524c5bb1b437bf51bbd3ea49129
JERTOLE <info@cryogas.kz>
2025-11-30 15:42:15 +0300

    Update local_storage.dart

--- mobile-wallet/lib/screens/services/local_storage.dart
+++ mobile-wallet/lib/screens/services/local_storage.dart
@@ -1,1 +1,18 @@
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorage {
+  LocalStorage._();
+  static final LocalStorage instance = LocalStorage._();
+
+  Future<SharedPreferences> get _prefs async => SharedPreferences.getInstance();
+
+  Future<String?> getString(String key) async {
+    final p = await _prefs;
+    return p.getString(key);
+  }
+
+  Future<void> setString(String key, String value) async {
+    final p = await _prefs;
+    await p.setString(key, value);
+  }
