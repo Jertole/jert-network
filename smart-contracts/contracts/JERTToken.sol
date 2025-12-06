c5ce7997d1ca940b9094a6349412add8b37659ca
JERTOLE <info@cryogas.kz>
2025-12-06 19:24:30 +0300

    Update JERTToken.sol

--- smart-contracts/contracts/JERTToken.sol
+++ smart-contracts/contracts/JERTToken.sol
@@ -13,3 +13,13 @@
 contract JERTToken {
     ...
 }
+
+/// @notice Transfers `amount` of JERT tokens to `to`.
+    /// @param to Recipient address.
+    /// @param amount Amount of tokens to transfer (in smallest units, decimals()).
+    /// @return success True if transfer succeeds.
+    function transfer(address to, uint256 amount) external returns (bool success) { ... }
+
+    /// @notice Returns the decimals used by the token.
+    /// @dev Used by off-chain systems to convert on-chain units into human-readable JERT amounts.
+    function decimals() external view returns (uint8) { ... }
