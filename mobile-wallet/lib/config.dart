b3ffc0e2571cdfe6de3b5230ce452092cb8ca709
JERTOLE <info@cryogas.kz>
2025-12-09 22:16:05 +0300

    Update config.dart

--- mobile-wallet/lib/config.dart
+++ mobile-wallet/lib/config.dart
@@ -1,8 +1,10 @@
 
-const String jertRpcUrl = 'http://127.0.0.1:8545'; // TODO: заменить на реальный RPC
+const String jertRpcUrl = 'http://127.0.0.1:8545'; // TODO: реальный RPC
 
-// TODO: сюда вставишь реальный адрес JERTToken после деплоя из deployments/addresses.json
+// TODO: сюда вставишь реальный адрес JERTToken после деплоя
 const String jertTokenAddress = '0x0000000000000000000000000000000000000000';
 
-// chainId твоей JERT-сети
 const int jertChainId = 1337;
+
+// API Gateway base URL
+const String jertApiBaseUrl = 'http://127.0.0.1:4000/api';
