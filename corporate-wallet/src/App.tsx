82589e9250e476f6e32cbc58d7c26cb0eb022cb2
JERTOLE <info@cryogas.kz>
2025-12-09 21:34:06 +0300

    Update App.tsx

--- corporate-wallet/src/App.tsx
+++ corporate-wallet/src/App.tsx
@@ -1,14 +1,13 @@
+
 import React from "react";
 import { Layout } from "./components/Layout";
 import { MultisigDashboard } from "./components/MultisigDashboard";
-import { TransactionsTable } from "./components/TransactionsTable";
 
 export const App: React.FC = () => {
   return (
     <Layout>
-      <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: 24 }}>
         <MultisigDashboard />
-        <TransactionsTable />
       </div>
     </Layout>
   );
