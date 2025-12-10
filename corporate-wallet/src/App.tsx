1b1dc6778564ae27acee92cffca4344c4d052390
JERTOLE <info@cryogas.kz>
2025-12-11 00:33:46 +0300

    Update App.tsx

--- corporate-wallet/src/App.tsx
+++ corporate-wallet/src/App.tsx
@@ -1,14 +1,9 @@
 
 import React from "react";
-import { Layout } from "./components/Layout";
-import { MultisigDashboard } from "./components/MultisigDashboard";
+import MultisigDashboard from "./components/MultisigDashboard";
 
-export const App: React.FC = () => {
-  return (
-    <Layout>
-      <div style={{ display: "flex", flexDirection: "column", gap: 24 }}>
-        <MultisigDashboard />
-      </div>
-    </Layout>
-  );
-};
+function App() {
+  return <MultisigDashboard />;
+}
+
+export default App;
