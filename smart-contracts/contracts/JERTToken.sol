4c462f022499cf216b165912548249859b4c2d1e
JERTOLE <info@cryogas.kz>
2025-12-10 22:47:34 +0300

    Update JERTToken.sol

--- smart-contracts/contracts/JERTToken.sol
+++ smart-contracts/contracts/JERTToken.sol
@@ -8,18 +8,18 @@ contract JERTToken is ERC20, Ownable {
     uint256 public constant MAX_SUPPLY = 1_000_000_000 ether;
 
     constructor(address treasury)
-        ERC20("JERT Token", "JERT")
-        Ownable(treasury)         // <-- ВАЖНО: передаём владельца базовому контракту
+        ERC20("JERT Utility Token", "JERT")
+        Ownable(treasury)
     {
         _mint(treasury, MAX_SUPPLY);
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
-        require(totalSupply() + amount <= MAX_SUPPLY, "JERT: MAX_SUPPLY exceeded");
+        require(totalSupply() + amount <= MAX_SUPPLY, "JERT: cap exceeded");
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) external onlyOwner {
-        _burn(_msgSender(), amount);
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
     }
 }
