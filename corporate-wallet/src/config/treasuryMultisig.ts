dfdfa9d57d096eb52853ca12c04c31fbc9d55a66
JERTOLE <info@cryogas.kz>
2025-12-11 01:24:35 +0300

    Update treasuryMultisig.ts

--- corporate-wallet/src/config/treasuryMultisig.ts
+++ corporate-wallet/src/config/treasuryMultisig.ts
@@ -1,1 +1,65 @@
 
+export const TREASURY_MULTISIG_ADDRESS =
+  "0xYourTreasuryMultisigAddressHere"; // сюда потом вставим реальный адрес
+
+export const TREASURY_MULTISIG_ABI = [
+  {
+    inputs: [],
+    name: "getOwners",
+    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numConfirmationsRequired",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getTransactionCount",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_txId", type: "uint256" }],
+    name: "getTransaction",
+    outputs: [
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "bytes", name: "data", type: "bytes" },
+      { internalType: "bool", name: "executed", type: "bool" },
+      { internalType: "uint256", name: "numConfirmations", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_to", type: "address" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+      { internalType: "bytes", name: "_data", type: "bytes" },
+    ],
+    name: "submitTransaction",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_txId", type: "uint256" }],
+    name: "confirmTransaction",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_txId", type: "uint256" }],
+    name: "executeTransaction",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
