81c932f7846e8251c1c408455a79def0a85039f4
JERTOLE <info@cryogas.kz>
2025-12-11 22:51:31 +0300

    Update wallet_service.dart

--- mobile-wallet/lib/services/wallet_service.dart
+++ mobile-wallet/lib/services/wallet_service.dart
@@ -1,1 +1,39 @@
 
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart' as http;
+
+class WalletService {
+  final Web3Client client;
+  final String privateKey;
+
+  WalletService({
+    required String rpcUrl,
+    required this.privateKey,
+  }) : client = Web3Client(rpcUrl, http.Client());
+
+  Credentials get _credentials => EthPrivateKey.fromHex(privateKey);
+
+  Future<EtherAmount> getBalance(String address) async {
+    final balance = await client.getBalance(
+      EthereumAddress.fromHex(address),
+    );
+    return balance;
+  }
+
+  Future<String> sendNative({
+    required String to,
+    required BigInt amountWei,
+  }) async {
+    final tx = Transaction(
+      to: EthereumAddress.fromHex(to),
+      value: EtherAmount.inWei(amountWei),
+    );
+
+    final txHash = await client.sendTransaction(
+      _credentials,
+      tx,
+      chainId: null,
+    );
+    return txHash;
+  }
+}
