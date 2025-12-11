d2e52706e012dff3a6669c92f5f7e57c93df93fc
JERTOLE <info@cryogas.kz>
2025-12-11 14:27:14 +0300

    Update Dashboard.tsx

--- corporate-wallet/src/pages/Dashboard.tsx
+++ corporate-wallet/src/pages/Dashboard.tsx
@@ -1,1 +1,11 @@
+import React from "react";
+import { JertBalance } from "../components/JertBalance";
 
+export const Dashboard: React.FC = () => {
+  return (
+    <div style={{ padding: 24 }}>
+      {/* Остальной UI */}
+      <JertBalance />
+    </div>
+  );
+};
