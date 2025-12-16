aec41eb09bca84712d177b487ae0fff1e5671368
JERTOLE <info@cryogas.kz>
2025-12-16 15:23:27 +0300

    Create address-registry.ts

---
+++ smart-contracts/config/address-registry.ts
@@ -0,0 +1,9 @@
+export const AddressRegistry = {
+  network: "sepolia",
+
+  deployer: "0x80a74fde59fb5977cc32584cac0168db81d9d4b3",
+
+  treasuryMultisig: "0x0617f015f91fb711b64deede795c2179ab8488a3",
+
+  authority: "0xcf10f00fac777cd25c8bdf59803c0187ac407604",
+};
